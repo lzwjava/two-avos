21fb6d51f17b1f09e20e94e5c34e8521c6fdb9db
lzwjava <lzwjava@gmail.com>
2014-05-27 22:45:30 +0800

    add

--- cloud/app.js
+++ cloud/app.js
@@ -7,7 +7,10 @@ app.set('views','cloud/views');   // 设置模板目录
 app.set('view engine', 'ejs');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
 
-// 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
+app.get('/',function(req,res){
+    res.write('/public/index.html');
+});
+
 app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
