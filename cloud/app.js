fed06789d6824ff26df5ffeabb145dda207ae322
lzwjava <lzwjava@gmail.com>
2014-05-28 20:42:57 +0800

    web update

--- cloud/app.js
+++ cloud/app.js
@@ -7,10 +7,17 @@ app.set('views','cloud/views');   // 设置模板目录
 app.set('view engine', 'ejs');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
 
+function webUpdate(res) {
+    res.redirect('/webUpdate.html');
+}
 app.get('/',function(req,res){
     //res.redirect('/index.html');
-    res.redirect('/webUpdate.html');
+    webUpdate(res);
 });
+
+app.get('/index.html',function(req,res)){
+    webUpdate(res);
+}
 
 app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
